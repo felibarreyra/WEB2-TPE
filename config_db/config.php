a53336838905ecbc34bfb235a5f7601f12264744
Felipe Barreyra <felipebarreyra@gmail.com>
2023-11-02 21:42:56 -0300

    agrego controllers, models, view, config db y router

---
+++ config_db/config.php
@@ -0,0 +1,7 @@
+<?php
+const MYSQL_USER = 'root';
+    const MYSQL_PASS = '';
+    const MYSQL_DB = 'db_tpe';
+    const MYSQL_HOST = 'localhost';
+    const JWT_KEY = 'claveToken';
+    const JWT_EXP = 3600; // expiracion del token
